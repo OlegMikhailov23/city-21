6966e511f0ee3c3244373dad40fc971153b193f1
Dkfolkin <dkfolkin@gmail.com>
2019-03-11 21:13:01 +0300

    добавлен слайдер и форма обр. связи

---
+++ source/js/owl.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+$('.intro-slider').owlCarousel({
+    loop:true,
+    dots:true,
+    items:1,
+    autoplay: false,
+    nav: false,
+    autoplaySpeed: 1000,
+    autoplayTimeout: 15000,
+    mouseDrag: true,
+    touchDrag: true,
+    margin: 0
+});
+
+});
